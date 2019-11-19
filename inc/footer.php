530b5716e926f63789a5e121a3a06cf5e8258b2c
julian <lsajkefgksghijs@gmail.com>
2019-11-19 08:39:53 -0500

    vista

--- inc/footer.php
+++ inc/footer.php
@@ -2,23 +2,7 @@
 <body>
   <div class="container">
     <!-- Example row of columns -->
-    <div class="row">
-      <div class="col-md-4">
-        <h2>categoria 1</h2>
-        <p>ñkjdshfsahg</p>
-        <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-      </div>
-      <div class="col-md-4">
-        <h2>categoria 2</h2>
-        <p>kjsdfkjsdkfjlkdf </p>
-        <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-      </div>
-      <div class="col-md-4">
-        <h2>Categoria 3</h2>
-        <p>kjsdhflksjdflkjsdjfakhsdgjash</p>
-        <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-      </div>
-    </div>
+    
 
     <hr>
 
